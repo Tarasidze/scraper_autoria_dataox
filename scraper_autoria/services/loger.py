fac94d522797253f3fef433651b9afee5e2503c0
Tarasidze <dtar84@ukr.net>
2024-01-24 17:49:49 +0200

    add loger

--- scraper_autoria/services/loger.py
+++ scraper_autoria/services/loger.py
@@ -3,7 +3,7 @@ import sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(level_name)s]: %(message)s",
+    format="[%(levelname)s]: %(message)s",
     handlers=[
         logging.FileHandler("scraper.log"),
         logging.StreamHandler(sys.stdout)
